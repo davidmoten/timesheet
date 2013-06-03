68ddba3d64c0a9e1ea393291bf8ca0270ca9db31
Dave Moten <davidmoten@gmail.com>
2013-06-03 20:48:23 +1000

    added appengine plugin and demo code

---
+++ src/main/java/com/github/davidmoten/LoginFilter.java
@@ -0,0 +1,55 @@
+package com.github.davidmoten;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+public class LoginFilter implements Filter {
+
+	private final UserService userService;
+
+	public LoginFilter() {
+		userService = UserServiceFactory.getUserService();
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		if (userService.getCurrentUser() != null) {
+			if ("true".equals(request.getParameter("logout")))
+				((HttpServletResponse) response).sendRedirect(userService
+						.createLogoutURL(((HttpServletRequest) request)
+								.getRequestURI()));
+			else
+				chain.doFilter(request, response);
+		} else if (response instanceof HttpServletResponse)
+			((HttpServletResponse) response).sendRedirect(userService
+					.createLoginURL(((HttpServletRequest) request)
+							.getRequestURI()));
+		else
+
+			throw new ServletException(
+					"Unauthorized access, unable to forward to login page");
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+}
---
+++ src/main/java/com/github/davidmoten/TimeServlet.java
@@ -0,0 +1,20 @@
+package com.github.davidmoten;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TimeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 8026282588720357161L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		super.doGet(req, resp);
+	}
+
+}
